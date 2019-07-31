fbc12879f1b61e53578ae59dda10a2fcae87bd3b
TieZhuLi <Tiezhu.Li@paat.com>
2019-07-31 14:21:13 +0800

    init

---
+++ src/main/java/com/example/demo/clientAndServer/client/ClientInitializer.java
@@ -0,0 +1,33 @@
+package com.example.demo.clientAndServer.client;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @Filename: FluxDemo /com.example.demo.clientAndServer
+ * @Description:
+ * @Author: Tiezhu.Li@paat.com/804082784@qq.com
+ * @Version: 1.0
+ * @CreateTime: 2019/6/17 14:15
+ * @History:
+ */
+public class ClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline =  ch.pipeline();
+        pipeline.addLast("lengthFieldBasedFrameDecoder",
+                new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 2, 0, 2));
+        pipeline.addLast("lengthFieldPrepender", new LengthFieldPrepender(3));
+        pipeline.addLast("stringDecoder", new StringDecoder(CharsetUtil.UTF_8));
+        pipeline.addLast("stringEncoder", new StringEncoder(CharsetUtil.UTF_8));
+
+        pipeline.addLast(new MyClientHandler());
+    }
+}
---
+++ src/main/java/com/example/demo/clientAndServer/client/MyClientHandler.java
@@ -0,0 +1,57 @@
+package com.example.demo.clientAndServer.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @Filename: FluxDemo /com.example.demo.clientAndServer
+ * @Description:
+ * @Author: Tiezhu.Li@paat.com/804082784@qq.com
+ * @Version: 1.0
+ * @CreateTime: 2019/6/17 14:16
+ * @History:
+ */
+public class MyClientHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress());
+        System.out.println(msg);
+        ctx.channel().writeAndFlush("to Server: 草泥马");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(System.currentTimeMillis() + "...........");
+        ctx.channel().writeAndFlush("来自于客户端的问候！");
+        System.out.println("client channel Active...");
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(System.currentTimeMillis() + "...........");
+        System.out.println("client hanlder added...");
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(System.currentTimeMillis() + "...........");
+        System.out.println("client channel register...");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client channel inactive...");
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client channel unregister...");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
